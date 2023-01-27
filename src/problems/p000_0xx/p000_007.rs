10a5f0f8c94a32db0d1cd9153952191162123ab8
Zhongdong Yang <zhongdong_y@outlook.com>
2023-01-27 19:37:42 +0800

    Update: KaTeX expression in problems #7

--- src/problems/p000_0xx/p000_007.rs
+++ src/problems/p000_0xx/p000_007.rs
@@ -2,7 +2,7 @@
 //!
 //! Given a signed 32-bit integer `x`, return `x` with its digits reversed.
 //! If reversing `x` causes the value to go outside the signed 32-bit integer
-//! range \[-2^31, 2^31 - 1\], then return 0.
+//! range $[-2^{31}, 2^{31} - 1]$, then return 0.
 //!
 //! Assume the environment does not allow you to store 64-bit integers (signed 
 //! or unsigned).
@@ -26,7 +26,7 @@
 //!
 //! Constraints:
 //!
-//! - `-2^31 <= x <= 2^31 - 1`
+//! - $-2^{31} \leqslant x \leqslant 2^{31} - 1$
 //!
 //! Source: <https://leetcode.com/problems/reverse-integer/description/>
 
