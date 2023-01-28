5116ea0ac2718dc861343299ea7bd9b90a7dc621
Zhongdong Yang <zhongdong_y@outlook.com>
2023-01-28 23:49:15 +0800

    Update: copy theme.css to output file

--- docsgen.sh
+++ docsgen.sh
@@ -5,3 +5,4 @@ mkdir -p ./docs
 rm -rf ./docs/*
 RUSTDOCFLAGS='--extend-css assets/extended.css --html-in-header assets/head.html --html-after-content assets/after-content.html' cargo doc
 cp -r target/doc/* ./docs/
+cp docs/theme.css docs/static.files/theme.css
