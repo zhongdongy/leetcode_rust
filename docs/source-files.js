de8e820650ee716a80a8fdb8316e2ad4985b1d2e
Zhongdong Yang <zhongdong_y@outlook.com>
2023-02-13 02:49:59 +0800

    Update: doc

--- docs/source-files.js
+++ docs/source-files.js
@@ -1,13 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"itoa":["",[],["lib.rs","udiv128.rs"]],\
-"leetcode_rust":["",[["problems",[["p000_0xx",[],["p000_005.rs","p000_006.rs","p000_007.rs","p000_008.rs","p000_009.rs","p000_010.rs","p000_011.rs","p000_012.rs","p000_013.rs","p000_014.rs","p000_015.rs"]]]],["problems_cn",[["p000_0xx",[],["p000_005.rs","p000_006.rs","p000_007.rs","p000_008.rs","p000_009.rs","p000_010.rs","p000_011.rs","p000_012.rs","p000_013.rs","p000_014.rs"]]]]],["common.rs","lib.rs","macros.rs","problems.rs","problems_cn.rs"]],\
-"proc_macro2":["",[],["detection.rs","fallback.rs","lib.rs","marker.rs","parse.rs","rcvec.rs","wrapper.rs"]],\
-"quote":["",[],["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]],\
-"ryu":["",[["buffer",[],["mod.rs"]],["pretty",[],["exponent.rs","mantissa.rs","mod.rs"]]],["common.rs","d2s.rs","d2s_full_table.rs","d2s_intrinsics.rs","digit_table.rs","f2s.rs","f2s_intrinsics.rs","lib.rs"]],\
-"serde":["",[["de",[],["format.rs","ignored_any.rs","impls.rs","mod.rs","seed.rs","utf8.rs","value.rs"]],["private",[],["de.rs","doc.rs","mod.rs","ser.rs","size_hint.rs"]],["ser",[],["fmt.rs","impls.rs","impossible.rs","mod.rs"]]],["integer128.rs","lib.rs","macros.rs"]],\
-"serde_derive":["",[["internals",[],["ast.rs","attr.rs","case.rs","check.rs","ctxt.rs","mod.rs","receiver.rs","respan.rs","symbol.rs"]]],["bound.rs","de.rs","dummy.rs","fragment.rs","lib.rs","pretend.rs","ser.rs","this.rs","try.rs"]],\
-"serde_json":["",[["features_check",[],["mod.rs"]],["io",[],["mod.rs"]],["value",[],["de.rs","from.rs","index.rs","mod.rs","partial_eq.rs","ser.rs"]]],["de.rs","error.rs","iter.rs","lib.rs","macros.rs","map.rs","number.rs","read.rs","ser.rs"]],\
-"syn":["",[["gen",[],["clone.rs","gen_helper.rs"]]],["attr.rs","await.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","drops.rs","error.rs","export.rs","expr.rs","ext.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]],\
-"unicode_ident":["",[],["lib.rs","tables.rs"]]\
+"leetcode_rust":["",[["problems",[["p000_0xx",[],["p000_005.rs","p000_006.rs","p000_007.rs","p000_008.rs","p000_009.rs","p000_010.rs","p000_011.rs","p000_012.rs","p000_013.rs","p000_014.rs","p000_015.rs"]]]],["problems_cn",[["p000_0xx",[],["p000_005.rs","p000_006.rs","p000_007.rs","p000_008.rs","p000_009.rs","p000_010.rs","p000_011.rs","p000_012.rs","p000_013.rs","p000_014.rs"]]]]],["common.rs","lib.rs","macros.rs","problems.rs","problems_cn.rs"]]\
 }');
 createSourceSidebar();
--- docsgen.sh
+++ docsgen.sh
@@ -3,7 +3,7 @@
 
 mkdir -p ./docs
 rm -rf ./docs/*
-RUSTDOCFLAGS='--extend-css assets/extended.css --html-in-header assets/head.html --html-after-content assets/after-content.html' cargo doc
+RUSTDOCFLAGS='--extend-css assets/extended.css --html-in-header assets/head.html --html-after-content assets/after-content.html' cargo doc --no-deps
 cp -r target/doc/* ./docs/
 cp docs/theme.css docs/static.files/theme.css
 cp -r assets/images ./docs/
