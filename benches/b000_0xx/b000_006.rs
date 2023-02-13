7cc6ec5eeb25599fd9b3af82c47bb21fda071656
Zhongdong Yang <zhongdong_y@outlook.com>
2023-02-13 19:27:12 +0800

    Fix: prepare test cases before starting benchmark

--- benches/b000_0xx/b000_006.rs
+++ benches/b000_0xx/b000_006.rs
@@ -3,9 +3,10 @@ use leetcode_rust::cases::c000_0xx::*;
 use leetcode_rust::problems::p000_0xx::*;
 
 pub fn zigzag_conversion(c: &mut Criterion) {
+    let cases = c000_006::use_cases();
     c.bench_function("ZigZag_Conversion", |b| {
         b.iter(|| {
-            c000_006::use_cases().iter().for_each(move |c| {
+            cases.iter().for_each(move |c| {
                 p000_006::zigzag_conversion(
                     black_box(c.input().clone()),
                     black_box(c.params[0]),
